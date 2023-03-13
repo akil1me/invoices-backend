26430942195203e0bebf86ddaa50d48c06b9a48b
akil1me <akipax123@gmail.com>
2023-03-14 04:01:56 +0500

    add

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ app.use(auth);
 
 app.use(router);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
