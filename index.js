7dfd989b07833a9fa68da04c862c75ca83f3a988
akil1me <akipax123@gmail.com>
2023-03-14 11:19:29 +0500

    fix

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ app.use(auth);
 app.use(router);
 
 const PORT = process.env.PORT || 3000;
-console.log(process.env.PORT);
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
